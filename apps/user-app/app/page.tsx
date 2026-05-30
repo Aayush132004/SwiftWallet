dad8263649f3ffb04ee8143e67035e071db00bb0
Aayush Sharma <aayushsharma132004@gmail.com>
2026-05-31 00:05:29 +0530

    bootstrap-phase1

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return(
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl text-amber-500 font-bold">Welcome to the User App</h1>
+      <p className="mt-4 text-lg">This is the user application for managing your wallet.</p>
+    </div>
+  )
+}
