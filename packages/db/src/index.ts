478424292e7618dea834cbd648c2b61a0cbdbda0
Aayush Sharma <aayushsharma132004@gmail.com>
2026-06-03 03:49:16 +0530

    next-autn and db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,15 @@
-export * from '@prisma/client';
+import { Pool } from "pg";
+import { PrismaClient } from "../generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export default prisma;
